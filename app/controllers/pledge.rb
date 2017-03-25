3a0d627d3a87b03185ef5e1539edf34d023011d2
Tim Luo <timoman@gmail.com>
2017-03-25 16:31:18 -0500

    endpoints for creating pledge

---
+++ app/controllers/pledge.rb
@@ -0,0 +1,28 @@
+##
+# {
+#  "pledge" : {
+#    "player_id" : "mlb-ben-zobrist",
+#    "stat_category" : "home_runs",
+#     "donation_amount" : 1,
+#     "user_id" : 1
+#   }
+# }
+post '/pledge' do
+  payload = params
+  payload = JSON.parse(request.body.read).symbolize_keys unless params[:path]
+
+  puts payload
+  @pledge = Pledge.new(payload[:pledge])
+  if @pledge.save
+    redirect '/pledges'
+  else
+    halt 500
+  end
+end
+
+get '/pledges' do
+  @pledges = Pledge.all
+  erb :pledges
+end
+
+
